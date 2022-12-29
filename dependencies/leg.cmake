959e5b160972729c2b49c140e39bcfb9d4f6f326
Mathias Klausen <26812242+MKKlausen@users.noreply.github.com>
2022-12-29 11:00:22 +0100

    external-leg-2

--- dependencies/leg.cmake
+++ dependencies/leg.cmake
@@ -4,6 +4,7 @@ FetchContent_Declare(
     leg
     PREFIX ${CMAKE_CURRENT_BINARY_DIR}/leg
     GIT_REPOSITORY https://github.com/Zaph-x/leg-assembly-loader
+    Git_TAG 2b6e503f13d769fb5fdf706f23ba12dc3bffe233
 )
 
 FetchContent_MakeAvailable (leg)
