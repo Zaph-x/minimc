6bd264b8f14ce10f3c4c8065511712a6f10eacce
Zaph-x <35431127+Zaph-x@users.noreply.github.com>
2023-03-22 10:12:39 +0100

    commit

---
+++ dependencies/ctl-expr.cmake
@@ -0,0 +1,15 @@
+include(FetchContent)
+find_package(Git REQUIRED)
+
+FetchContent_Declare(ctl
+  SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/ctl-src
+  BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/ctl-build
+  GIT_REPOSITORY https://github.com/sillydan1/ctl-expr.git
+  GIT_TAG v2.0.0
+)
+
+if (NOT ctl_POPULATED)
+  FetchContent_Populate(ctl)
+  add_subdirectory(${ctl_SOURCE_DIR} ${ctl_BINARY_DIR})
+endif()
+
