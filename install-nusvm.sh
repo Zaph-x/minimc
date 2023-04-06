25cb47ee2969ff0af7373f5f2abc8185f854cdff
Zaph-x <35431127+Zaph-x@users.noreply.github.com>
2023-04-06 12:18:48 +0200

    Added flags and acted on them

--- install-nusvm.sh
+++ install-nusvm.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
 
-# Check if user is root
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
-
 # check if git is installed
 if ! [ -x "$(command -v git)" ]; then
   echo 'Error: git is not installed.' >&2
@@ -29,7 +23,7 @@ sh build.sh
 # Prompt user if they wish to install NuSMV to /usr/local/bin
 read -p "Do you wish to install NuSMV and ltl2smv to /usr/local/bin? (y/n) " yn
 case $yn in
-    [Yy]* ) install -m 755 nusmv /usr/local/bin; install -m 755 ltl2smv /usr/local/bin; break;;
+    [Yy]* ) sudo install -m 755 nusmv /usr/local/bin; sudo install -m 755 ltl2smv /usr/local/bin; break;;
     [Nn]* ) exit;;
     * ) echo "Please answer yes or no.";;
 esac
