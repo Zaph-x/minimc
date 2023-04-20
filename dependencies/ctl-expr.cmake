c002de7b8a96efae992eb187e7883eb68b440761
christofferaaen <cbaaen@live.dk>
2023-04-20 13:18:26 +0200

    Asger main man

--- dependencies/ctl-expr.cmake
+++ dependencies/ctl-expr.cmake
@@ -5,7 +5,7 @@ FetchContent_Declare(ctl
   SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/ctl-src
   BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/ctl-build
   GIT_REPOSITORY https://github.com/sillydan1/ctl-expr.git
-  GIT_TAG v2.0.0
+  GIT_TAG v2.0.1
 )
 
 if (NOT ctl_POPULATED)
